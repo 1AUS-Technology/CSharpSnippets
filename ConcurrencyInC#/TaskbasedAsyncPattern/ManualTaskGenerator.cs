42402f473e362dad186ab945ef2884b767821013
Toan Nguyen <toan.nguyen@qr.com.au>
2024-04-29 11:25:03 +1000

    Manual add the task generation

---
+++ ConcurrencyInC#/TaskbasedAsyncPattern/ManualTaskGenerator.cs
@@ -0,0 +1,24 @@
+﻿namespace ConcurrencyInC_.TaskbasedAsyncPattern;
+
+public  static class ManualTaskGenerator
+{
+    public static Task<int> ReadStreamAsync(this Stream stream, byte[] buffer, int offset, int count)
+    {
+       TaskCompletionSource<int> tcs = new TaskCompletionSource<int>();
+
+       stream.BeginRead(buffer, offset, count, iar =>
+         {
+              try
+              {
+                int bytesRead = stream.EndRead(iar);
+                tcs.SetResult(bytesRead);
+              }
+              catch (Exception exc)
+              {
+                tcs.SetException(exc);
+              }
+         }, null);
+
+       return tcs.Task;
+    }
+}
