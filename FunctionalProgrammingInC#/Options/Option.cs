60835f3513fba39f64351ead1bd940fd27948b04
Toan Nguyen <toan.nguyen@qr.com.au>
2024-04-30 14:25:40 +1000

    Add option

---
+++ FunctionalProgrammingInC#/Options/Option.cs
@@ -0,0 +1,8 @@
+﻿namespace FunctionalProgrammingInC_.Options;
+
+public interface Option<T> { }
+record None : Option<T>;
+record Some<T>(T Value) : Option<T>;
+
+record None<T> : Option<T>;
+
---
+++ FunctionalProgrammingInC#/Options/OptionMatcherExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace FunctionalProgrammingInC_.Options;
+
+public static class OptionMatcherExtensions
+{
+    public static R Match<T, R>(this Option<T> opt, Func<R> None, Func<T, R> Some)
+    {
+        return opt switch
+        {
+            None<T> => None(),
+            Some<T>(var t) => Some(t),
+            _ => throw new ArgumentException("Option must be None or Some")
+        };
+    }
+}
