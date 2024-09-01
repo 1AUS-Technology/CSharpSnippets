6b5fc4fe2f95c299564be1d42a4ef61eb10cff53
Toan Nguyen <toan.nguyen@qr.com.au>
2024-09-02 09:24:17 +1000

    Add the host life cycle

---
+++ AspNetCoreRecap/HostBuilderServices/HostedServiceWithLifetime.cs
@@ -0,0 +1,40 @@
+﻿namespace AspNetCoreRecap.HostBuilderServices;
+
+public class HostedServiceWithLifetime (IHostApplicationLifetime appLifetime, ILogger<HostedServiceWithLifetime> logger) : IHostedService, IHostedLifecycleService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Application startAsync");
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Application stop async");
+        return Task.CompletedTask;
+    }
+
+    public Task StartedAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Application started");
+        return Task.CompletedTask;
+    }
+
+    public Task StartingAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Application starting");
+        return Task.CompletedTask;
+    }
+
+    public Task StoppedAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Application stopped");
+        return Task.CompletedTask;
+    }
+
+    public Task StoppingAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Application stopping");
+        return Task.CompletedTask;
+    }
+}
