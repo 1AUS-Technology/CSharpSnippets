d4cf96f1379ed6031926b678cc332551fccda8ee
Toan Nguyen <toan.nguyen@qr.com.au>
2024-10-01 11:52:21 +1000

    Print certificates

---
+++ CertificateManagements/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CertificateManagements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintStoreNameAndLocations();
+
+            string storeName = "Root";
+            PrintStoreCertificates(storeName, StoreLocation.CurrentUser);
+        }
+
+        private static void PrintStoreCertificates(string storeName, StoreLocation location)
+        {
+            var store = new X509Store(storeName, location);
+            store.Open(OpenFlags.ReadOnly);
+
+            foreach (var storeCertificate in store.Certificates)
+            {
+                Console.WriteLine("Certificate Name: {0}", storeCertificate.IssuerName.Name);
+            }
+
+        }
+
+        private static void PrintStoreNameAndLocations()
+        {
+            Console.WriteLine("\r\nExists Certs Name and Location");
+            Console.WriteLine("------ ----- -------------------------");
+
+            foreach (StoreLocation storeLocation in (StoreLocation[])
+                     Enum.GetValues(typeof(StoreLocation)))
+            {
+                foreach (StoreName storeName in (StoreName[])
+                         Enum.GetValues(typeof(StoreName)))
+                {
+                    X509Store store = new X509Store(storeName, storeLocation);
+
+                    try
+                    {
+                        store.Open(OpenFlags.OpenExistingOnly);
+
+                        Console.WriteLine("Yes    {0,4}  {1}, {2}",
+                            store.Certificates.Count, store.Name, store.Location);
+                    }
+                    catch (CryptographicException)
+                    {
+                        Console.WriteLine("No           {0}, {1}",
+                            store.Name, store.Location);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ SecurityIssuesWithApi/Controllers/XssController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace SecurityIssuesWithApi.Controllers;
+
+public class XssController : ControllerBase
+{
+    [HttpGet]
+    public ContentResult BadSearchAPI(string searchTerm)
+    {
+        var results = new List<string>
+        {
+            // Not sanitized al vulnerable to XSS
+            searchTerm + " 1",
+            searchTerm + " 2",
+            searchTerm + " 3"
+        };
+        return new ContentResult
+        {
+            ContentType = "text/html",
+            Content = $"[\"{string.Join("\", \"", results)}\"]"
+        };
+    }
+
+    [HttpGet]
+    public ContentResult GoodSearchAPI(string searchTerm)
+    {
+        // Sanitize the search term
+        var sanitizedSearchTerm = System.Web.HttpUtility.JavaScriptStringEncode(searchTerm);
+
+        var results = new List<string>
+        {
+            sanitizedSearchTerm + " 1",
+            sanitizedSearchTerm + " 2",
+            sanitizedSearchTerm + " 3"
+        };
+
+        return new ContentResult
+        {
+            ContentType = "application/json",
+            Content = System.Text.Json.JsonSerializer.Serialize(results)
+        };
+    }
+}
