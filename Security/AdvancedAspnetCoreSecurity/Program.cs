beb452d1839bd8147ecf501a7164abf27e1abfd4
Toan Nguyen <toan.nguyen@qr.com.au>
2024-10-14 13:19:19 +1000

    Add encryption and decryption

--- Security/AdvancedAspnetCoreSecurity/Program.cs
+++ Security/AdvancedAspnetCoreSecurity/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace AdvancedAspnetCoreSecurity
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AdvancedAspnetCoreSecurity
 {
     internal class Program
     {
+        private const string InitialisationVector = "zP6xLOlBdTThKmXX";
+
+        /// <summary>
+        /// The key to use when encrypting and decrypting strings. If this value ever changes then any strings
+        /// that have been previously encrypted will not be able to be decrypted
+        /// </summary>
+        private static readonly byte[] EncryptionKey = [40, 137, 218, 23, 105, 231, 179, 42, 89, 102, 126, 154, 218, 32, 126, 240, 45, 247, 110, 141, 198, 0, 226, 57];
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (RijndaelManaged myRijndael = new RijndaelManaged())
+            {
+                myRijndael.BlockSize = 128;
+                myRijndael.KeySize = 192;
+
+                myRijndael.GenerateKey();
+                myRijndael.GenerateIV();
+
+                byte[] iv = myRijndael.IV;
+
+                var hex = myRijndael.IV.BytesToHex();
+
+                Console.WriteLine("IV Generated: " + hex);
+                Console.WriteLine($"QR: {InitialisationVector}");
+
+                Console.WriteLine("IV Length");
+
+                Console.WriteLine("IV Generated : " + hex.Length );
+
+                Console.WriteLine("QR Length: " + InitialisationVector.Length);
+
+                var key = myRijndael.Key;
+
+                Console.WriteLine("Key Length: " + key.Length);
+                Console.WriteLine("QR Key Length: " + EncryptionKey.Length);
+
+                Console.WriteLine("Key:");
+
+                foreach (byte b in key)
+                {
+                    Console.Write(b + ",");
+                }
+
+            }
         }
     }
 }
--- Security/AdvancedAspnetCoreSecurity/SymmetricEncryptor.cs
+++ Security/AdvancedAspnetCoreSecurity/SymmetricEncryptor.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography;
+using System.Text;
 using Microsoft.Extensions.Configuration;
 
 namespace AdvancedAspnetCoreSecurity;
@@ -9,9 +10,12 @@ public class SymmetricEncryptor
     {
         AES128 = 1
     }
-    private EncryptionAlgorithm _defaultAlgorithm = EncryptionAlgorithm.AES128;
+
+    private readonly EncryptionAlgorithm _defaultAlgorithm = EncryptionAlgorithm.AES128;
+
     private readonly int _defaultKeyIndex;
     private readonly ISecretStore _secretStore;
+
     public SymmetricEncryptor(IConfiguration config, ISecretStore secretStore)
     {
         _defaultKeyIndex = config.GetValue<int>("AppSettings:KeyIndex");
@@ -25,7 +29,7 @@ public class SymmetricEncryptor
             case EncryptionAlgorithm.AES128:
                 return 16;
             default:
-                throw new NotImplementedException($"Cannot find block size for{ algorithm.ToString() } algorithm");
+                throw new NotImplementedException($"Cannot find block size for{algorithm.ToString()} algorithm");
         }
     }
 
@@ -42,20 +46,94 @@ public class SymmetricEncryptor
         }
     }
 
-    private string EncryptAES(string plainText, string key, EncryptionAlgorithm algorithm, int keyIndex)
+    private string EncryptAES(string plainText, string encryptionKey, EncryptionAlgorithm algorithm, int keyIndex)
     {
-        var keyBytes = key.HexStringToByteArray();
-        var initialVector = GenerateRandomBytes(GetBlockSizeInBytes(algorithm));
+        var keyBytes = encryptionKey.HexStringToByteArray();
+        int ivLength = AesGcm.NonceByteSizes.MaxSize;
+        int tagSize = AesGcm.TagByteSizes.MaxSize;
 
+        var initialVector = GenerateRandomBytes(ivLength);
+        byte[] tag = new byte[tagSize];
 
         byte[] encryptedBytes = [];
-
+        var plainBytes = Encoding.UTF8.GetBytes(plainText);
+        var cipherBytes = new byte[plainBytes.Length];
+        using (var aes = new AesGcm(keyBytes, tagSize))
+        {
+            aes.Encrypt(initialVector, plainBytes, cipherBytes, tag);
+        }
 
         var encryptedText = encryptedBytes.BytesToHex();
         var ivText = initialVector.BytesToHex();
+        var tagText = tag.BytesToHex();
 
         // concatenate all key and IV information for decryption
-        return $"[{(int)algorithm},{keyIndex}]{ivText}{encryptedText}";
+        return $"[{(int)algorithm}.{keyIndex}][{ivText}.{tagText}]{encryptedText}";
+    }
+
+
+    public string Decrypt(string cipherText, string keyName)
+    {
+        (int algorithm, int keyIndex, string initialVector, string tagText, string trimmedCipherText) = GetAlgorithm(cipherText);
+        var encryptionKey = _secretStore.GetKey(keyName, keyIndex);
+
+        var keyBytes = encryptionKey.HexStringToByteArray();
+        var tagBytes = tagText.HexStringToByteArray();
+
+        var encryptedBytes = trimmedCipherText.HexStringToByteArray();
+        byte[] plainText = new byte[encryptedBytes.Length];
+        using (var aes = new AesGcm(keyBytes, tagBytes.Length))
+        {
+            aes.Decrypt(initialVector.HexStringToByteArray(), encryptedBytes, tagBytes, plainText);
+        }
+
+        return Encoding.UTF8.GetString(plainText);
+    }
+
+    protected (int algorithm, int keyIndex, string initialVector, string tagText, string trimmedCipherText) GetAlgorithm(string cipherText)
+    {
+        if (cipherText.Length <= 5 || cipherText[0] != '[')
+        {
+            throw new ArgumentException("Rubbish encrypted data");
+        }
+
+        var firstClosingBracketLocation = cipherText.IndexOf(']', StringComparison.OrdinalIgnoreCase);
+        var cipherInformation = cipherText.Substring(1, firstClosingBracketLocation - 1).Split(".");
+
+        int algorithm;
+        if (int.TryParse(cipherInformation[0], out var foundAlgorithm))
+        {
+            algorithm = foundAlgorithm;
+        }
+        else
+        {
+            throw new ArgumentException("Invalid encrypted data. Cannot parse algorithm");
+        }
+
+        int keyIndex;
+        if (cipherInformation.Length == 2 && int.TryParse(cipherInformation[1], out var foundKeyIndex))
+        {
+            keyIndex = foundKeyIndex;
+        }
+        else
+        {
+            throw new ArgumentException("Invalid encrypted data. Cannot parse key index");
+        }
+
+        var secondBracketOpeningPosition = firstClosingBracketLocation + 1;
+
+        var secondBracketClosingPosition = cipherText.IndexOf(']', secondBracketOpeningPosition);
+
+        var ivAndTag = cipherText.Substring(secondBracketClosingPosition + 1, secondBracketClosingPosition - 1).Split(".");
+
+        string tagText;
+        var initialVector = ivAndTag[0];
+        tagText = ivAndTag[1];
+
+        string trimmedCipherText = cipherText.Substring(secondBracketClosingPosition + 1);
+
+
+        return (algorithm, keyIndex, initialVector, tagText, trimmedCipherText);
     }
 
     private byte[] GenerateRandomBytes(int blockSize)
