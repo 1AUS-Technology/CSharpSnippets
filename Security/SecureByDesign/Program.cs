8ce011a8f63fffe06ce905237a31f2c29fb2d30b
Toan Nguyen <toan.nguyen@qr.com.au>
2024-10-18 11:41:37 +1000

    Add secure by design

---
+++ Security/SecureByDesign/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SecureByDesign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
