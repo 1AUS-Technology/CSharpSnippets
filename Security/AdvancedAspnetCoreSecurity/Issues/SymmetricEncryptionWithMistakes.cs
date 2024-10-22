2b9a337f4d817f4f63fa3bd6d5fbebeaea5ef6d8
Toan Nguyen <sr.nguyenngoctoan@gmail.com>
2024-10-23 07:04:09 +1000

    Add encryption mistakes

---
+++ Security/AdvancedAspnetCoreSecurity/Issues/SymmetricEncryptionWithMistakes.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AdvancedAspnetCoreSecurity.Issues;
+
+public class SymmetricEncryptionWithMistakes
+{
+    public static byte[] Encrypt(string plainText, byte[] encryptionKey, byte[] initialVector)
+    {
+        byte[] inputBytes = Encoding.UTF8.GetBytes(plainText);
+
+        AesManaged aes = new AesManaged();
+        
+        ICryptoTransform encryptor = aes.CreateEncryptor(encryptionKey, initialVector);
+
+        using var memoryStream = new MemoryStream();
+        using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+        cryptoStream.Write(inputBytes, 0, inputBytes.Length);
+        cryptoStream.FlushFinalBlock();
+        return memoryStream.ToArray();
+    }
+
+    public static string Decrypt(byte[] encryptedData, byte[] encryptionKey, byte[] initialVector)
+    {
+        AesManaged aes = new AesManaged();
+
+        ICryptoTransform encryptor = aes.CreateDecryptor(encryptionKey, initialVector);
+
+        using var memoryStream = new MemoryStream();
+        using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+        cryptoStream.Write(encryptedData, 0, encryptedData.Length);
+        cryptoStream.FlushFinalBlock();
+
+        byte[] decryptedBytes = memoryStream.ToArray();
+
+        return Encoding.UTF8.GetString(decryptedBytes);
+    }
+}
--- Security/AdvancedAspnetCoreSecurity/Program.cs
+++ Security/AdvancedAspnetCoreSecurity/Program.cs
@@ -1,8 +1,36 @@
-﻿namespace AdvancedAspnetCoreSecurity
+﻿using System.Text;
+using AdvancedAspnetCoreSecurity.Issues;
+
+namespace AdvancedAspnetCoreSecurity
 {
     internal class Program
     {
         private static void Main(string[] args)
+        {
+
+            (string encryptionKey, string initialVector) = ReadKeyInformationFromConfigurationFile();
+
+            string plainText = "This is a secret message";
+            byte[] keyBytes = Convert.FromBase64String(encryptionKey);
+            byte[] ivBytes = Convert.FromBase64String(initialVector);
+
+            byte[] encryptedBytes = SymmetricEncryptionWithMistakes.Encrypt(plainText,keyBytes , ivBytes);
+
+            string decryptedText = SymmetricEncryptionWithMistakes.Decrypt(encryptedBytes, keyBytes, ivBytes);
+            
+            Console.WriteLine($"Plain text: {plainText}");
+            Console.WriteLine($"Encrypted (Base64): { Convert.ToBase64String(encryptedBytes)}");
+            Console.WriteLine($"Decrypted text: {decryptedText}");
+
+            Console.ReadLine();
+        }
+
+        private static (string encryptionKey, string initialVector) ReadKeyInformationFromConfigurationFile()
+        {
+            return ("vbq2dvHswgEa2N2fSnqt2Ri3C/JOhj4WHNTsq2FetOE=", "2nWjCsQjC65l+ggTcnZ8iA==");
+        }
+
+        private static void RunUpdatedEncryption()
         {
             var plainText = "This is a secret message";
 
@@ -20,8 +48,6 @@
 
             Console.WriteLine($"Original text:  {plainText,-30}");
             Console.WriteLine($"Decrypted text: {decryptedText,-30}");
-
-            Console.ReadLine();
         }
     }
 }
