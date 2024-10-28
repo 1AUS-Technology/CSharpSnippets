82aed5bcd099eee96107a8a8bf2e97e5ae8336ab
Toan Nguyen <toan.nguyen@qr.com.au>
2024-10-28 12:44:06 +1000

    Add MVC identity

---
+++ Security/EcommerceUsingIdentity/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using EcommerceUsingIdentity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceUsingIdentity.Controllers;
+
+public class HomeController
+{
+    private ProductDbContext DbContext;
+    public HomeController(ProductDbContext ctx) => DbContext = ctx;
+    public IActionResult Index() => View(DbContext.Products);
+}
---
+++ Security/EcommerceUsingIdentity/Program.cs
@@ -0,0 +1,50 @@
+using EcommerceUsingIdentity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceUsingIdentity
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ProductDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ProductDbContext>();
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapRazorPages()
+               .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
