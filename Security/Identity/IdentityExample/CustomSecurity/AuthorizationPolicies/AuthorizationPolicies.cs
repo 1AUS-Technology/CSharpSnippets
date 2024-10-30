4b777076caf11a9ef3498b127490cae0c84126c0
Toan Nguyen <toan.nguyen@qr.com.au>
2024-10-30 16:19:40 +1000

    Add authorization policies

--- Security/Identity/IdentityExample/CustomSecurity/AuthorizationPolicies/AuthorizationPolicies.cs
+++ Security/Identity/IdentityExample/CustomSecurity/AuthorizationPolicies/AuthorizationPolicies.cs
@@ -1,4 +1,5 @@
-﻿using IdentityExample.CustomSecurity.AuthorizationHandlers;
+﻿using System.Security.Claims;
+using IdentityExample.CustomSecurity.AuthorizationHandlers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authorization.Infrastructure;
 
@@ -18,5 +19,14 @@ public class AuthorizationPolicies
             new NameAuthorizationRequirement("Cuti"),
             new AssertionRequirement(context => !string.Equals(context.User.Identity?.Name, "VanBeo", StringComparison.OrdinalIgnoreCase))
         }, [AuthenticationSchemes.TestScheme]);
+
+        options.AddPolicy("UsersExceptBob", new AuthorizationPolicy(
+            new IAuthorizationRequirement[] {
+                new RolesAuthorizationRequirement(new[] { "User" }),
+                new AssertionRequirement(context => !string.Equals(context.User.Identity?.Name, "Bob", StringComparison.OrdinalIgnoreCase))
+            }, Enumerable.Empty<string>()));
+
+        options.AddPolicy("AdminOnly", policyConfiguration=> policyConfiguration.RequireAuthenticatedUser()
+            .RequireClaim(ClaimTypes.Role, "Administrator"));
     }
 }
