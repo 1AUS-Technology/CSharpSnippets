982335de9dfe4d29e9fdcece5308b6f29a953aef
Toan Nguyen <sr.nguyenngoctoan@gmail.com>
2024-10-31 08:35:53 +1000

    Pattern matching

---
+++ CSharpSnippets/CsharpRecap/PatternMatching/HousePriceCalculationUsingIf.cs
@@ -0,0 +1,100 @@
+﻿namespace CsharpRecap.PatternMatching;
+
+public class HousePriceCalculationUsingIf
+{
+    public decimal CalculateHousePrice(House house)
+    {
+        if (house.LotSize >= 500)
+        {
+            if (house.NumberOfBedRooms > 4)
+            {
+                return 1.5m;
+            }
+
+            if (house.NumberOfBedRooms == 4)
+            {
+                if (house.NumberOfBathRooms >= 2)
+                {
+                    return 1.1m;
+                }
+                else
+                {
+                    return 1m;
+                }
+            }
+
+            if (house.NumberOfBedRooms == 3)
+            {
+                if (house.NumberOfBathRooms >= 2)
+                {
+                    return 0.9m;
+                }
+                else
+                {
+                    return 0.8m;
+                }
+            }
+            else
+            {
+                if (house.NumberOfBathRooms >= 2)
+                {
+                    return 0.7m;
+                }
+                else
+                {
+                    return 0.6m;
+                }
+            }
+        }
+        else
+        {
+            if (house.NumberOfBedRooms > 4)
+            {
+                return 1.3m;
+            }
+
+            if (house.NumberOfBedRooms == 4)
+            {
+                if (house.NumberOfBathRooms >= 2)
+                {
+                    return 1.0m;
+                }
+                else
+                {
+                    return 0.9m;
+                }
+            }
+
+            if (house.NumberOfBedRooms == 3)
+            {
+                if (house.NumberOfBathRooms >= 2)
+                {
+                    return 0.8m;
+                }
+                else
+                {
+                    return 0.7m;
+                }
+            }
+            else
+            {
+                if (house.NumberOfBathRooms >= 2)
+                {
+                    return 0.6m;
+                }
+                else
+                {
+                    return 0.5m;
+                }
+            }
+        }
+    }
+}
+
+public class House
+{
+    public required uint LotSize { get; set; }
+    public required uint NumberOfBedRooms { get; set; }
+
+    public required uint NumberOfBathRooms { get; set; }
+}
---
+++ CSharpSnippets/CsharpRecap/PatternMatching/HousePriceUsingPatternMatching.cs
@@ -0,0 +1,26 @@
+﻿namespace CsharpRecap.PatternMatching;
+
+public class HousePriceUsingPatternMatching
+{
+    public decimal CalculateHousePrice(House house)
+    {
+        return (house.LotSize, house.NumberOfBedRooms, house.NumberOfBathRooms) switch
+        {
+            (>= 500, > 4, _) => 1.5m,
+            (>= 500, 4, >= 2) => 1.1m,
+            (>= 500, 4, _) => 1m,
+            (>= 500, 3, >= 2) => 0.9m,
+            (>= 500, 3, _) => 0.8m,
+            (>= 500, _, >= 2) => 0.7m,
+            (>= 500, _, _) => 0.6m,
+
+            (_, > 4, _) => 1.3m,
+            (_, 4, >= 2) => 1.0m,
+            (_, 4, _) => 0.9m,
+            (_, 3, >= 2) => 0.8m,
+            (_, 3, _) => 0.7m,
+            (_, _, >= 2) => 0.6m,
+            (_, _, _) => 0.5m,
+        };
+    }
+}
