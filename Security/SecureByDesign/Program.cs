759e178473909dee33da132048d280c120a5347e
Toan Nguyen <sr.nguyenngoctoan@gmail.com>
2024-11-19 15:28:51 +1000

    Add secure by design

--- Security/SecureByDesign/Program.cs
+++ Security/SecureByDesign/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace SecureByDesign
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecureByDesign
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var qty = new OrderQuantity(-1);
+            Console.WriteLine(qty);
+
+        }
+
+
+
+        private static void ValidateOrder(string customerName, int quantity)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(customerName);
+
+            if (quantity < 1 || quantity > 1000_000)
+            {
+                string message = $"Quantity must be between {1} and {1000_000}";
+                throw new ArgumentOutOfRangeException(nameof(quantity), message);
+            }
+        }
+
+        private static void UpdateOrder(string customerName, OrderQuantity updatedQuantity)
+        {
+            Console.WriteLine("Order updated");
+
+        }
+
+        private static void DispatchOrder(string customerName, OrderQuantity quantity)
+        {
+            Console.WriteLine("Order dispatched");
         }
     }
+
+    public record struct OrderQuantity
+    {
+        public OrderQuantity(int quantity)
+        {
+            if (quantity < 1 || quantity > 1000_000)
+            {
+                string message = $"Quantity must be between {1} and {1000_000}";
+                throw new ArgumentOutOfRangeException(nameof(quantity), message);
+            }
+            Quantity = quantity;
+        }
+
+        public int Quantity { get; }
+    }
 }
+
+
+
