5288903a6ee70650e94d1cdc9a9b2bd30a957aeb
Toan Nguyen <sr.nguyenngoctoan@gmail.com>
2024-11-24 10:12:36 +1000

    House price example

---
+++ CSharpSnippets/CsharpRecap/CodingMistakes/House.cs
@@ -0,0 +1,8 @@
+﻿namespace CsharpRecap.CodingMistakes;
+
+public class House
+{
+    public uint BathroomCount { get; set; }
+    public uint BedroomCount { get; set; }
+    public decimal EstimatedPrice { get; set; }
+}
---
+++ CSharpSnippets/CsharpRecap/CodingMistakes/RealestateAgent.cs
@@ -0,0 +1,17 @@
+﻿namespace CsharpRecap.CodingMistakes;
+
+public class RealEstateAgent
+{
+    public decimal EstimateHousePrice(House house)
+    {
+        decimal price = 0;
+
+        price += house.BathroomCount * 10000;
+        price += house.BedroomCount * 20000;
+        price += house.BedroomCount * 50000;
+
+        house.EstimatedPrice = price;
+
+        return price;
+    }
+}
